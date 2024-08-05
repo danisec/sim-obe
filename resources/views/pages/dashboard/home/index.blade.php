c6db24d2846e4e9ba099232ee6c064d28654f6d1
danisec <daniaprilyanto35@gmail.com>
2024-08-05 16:14:05 +0700

    update: pages/dashboard/home/index

--- resources/views/pages/dashboard/home/index.blade.php
+++ resources/views/pages/dashboard/home/index.blade.php
@@ -1,4 +1,4 @@
-<x-layouts.app-dashboard title="{{ $title }}">
+<x-app-dashboard title="{{ $title }}">
 
     <div class="my-8">
         <h4 class="mb-6 text-xl font-semibold text-gray-900">Predikat Capaian Pembelajaran Lulusan</h4>
@@ -21,50 +21,19 @@
             </thead>
 
             <tbody>
-                <tr class="border-b bg-white hover:bg-slate-100">
-                    <th class="whitespace-nowrap px-6 py-4 font-medium text-gray-900" scope="row">
-                        1
-                    </th>
-                    <td class="px-6 py-4">
-                        85 - 100
-                    </td>
-                    <td class="px-6 py-4">
-                        Sangat kompeten (Exemplary)
-                    </td>
-                </tr>
-                <tr class="border-b bg-white hover:bg-slate-100">
-                    <th class="whitespace-nowrap px-6 py-4 font-medium text-gray-900" scope="row">
-                        2
-                    </th>
-                    <td class="px-6 py-4">
-                        75 - 84
-                    </td>
-                    <td class="px-6 py-4">
-                        Kompeten (Competent)
-                    </td>
-                </tr>
-                <tr class="border-b bg-white hover:bg-slate-100">
-                    <th class="whitespace-nowrap px-6 py-4 font-medium text-gray-900" scope="row">
-                        3
-                    </th>
-                    <td class="px-6 py-4">
-                        60 - 74
-                    </td>
-                    <td class="px-6 py-4">
-                        Berkembang (Developing)
-                    </td>
-                </tr>
-                <tr class="border-b bg-white hover:bg-slate-100">
-                    <th class="whitespace-nowrap px-6 py-4 font-medium text-gray-900" scope="row">
-                        4
-                    </th>
-                    <td class="px-6 py-4">
-                        0 - 59
-                    </td>
-                    <td class="px-6 py-4">
-                        Tidak memuaskan (Unstatisfactory)
-                    </td>
-                </tr>
+                @foreach ($predikatCPL as $itemPredikat)
+                    <tr class="border-b bg-white hover:bg-slate-100">
+                        <th class="whitespace-nowrap px-6 py-4 font-medium text-gray-900" scope="row">
+                            {{ $loop->iteration }}
+                        </th>
+                        <td class="px-6 py-4">
+                            {{ $itemPredikat['range'] }}
+                        </td>
+                        <td class="px-6 py-4">
+                            {{ $itemPredikat['predikat'] }}
+                        </td>
+                    </tr>
+                @endforeach
             </tbody>
         </table>
     </div>
@@ -99,138 +68,52 @@
                 </tr>
             </thead>
 
-            <tbody>
-                <tr class="border-b bg-white hover:bg-slate-100">
-                    <th class="whitespace-nowrap px-6 py-4 font-medium text-gray-900" scope="row">
-                        23-MKU-CPL-01
-                    </th>
-                    <td class="px-6 py-4">
-                        Bertakwa kepada Tuhan Yang Maha Esa, taat hukum dan disiplin dalam kehidupan bermasyarakat dan
-                        bernegara
-                    </td>
-                    <td class="px-6 py-4">
-                        {{ $scoreCPL['23-MKU-CPL-01'] }}
-                    </td>
-                    <td class="px-6 py-4">
-                        {{ $scoreCPL['23-MKU-CPL-01'] >= 85
-                            ? 'Sangat kompeten (Exemplary)'
-                            : ($scoreCPL['23-MKU-CPL-01'] >= 75
-                                ? 'Kompeten (Competent)'
-                                : ($scoreCPL['23-MKU-CPL-01'] >= 60
-                                    ? 'Berkembang (Developing)'
-                                    : 'Tidak memuaskan (Unstatisfactory)')) }}
-                    </td>
-                    <td class="px-6 py-4">
-                        Sikap
-                    </td>
-                    <td class="px-6 py-4">
-                        <p>Perilaku</p>
-                        <br />
-                        <p>Bersikap Inklusif</p>
-                        <br />
-                        <p>Patuh</p>
-                        <br />
-                        <p>Disiplin</p>
-                    </td>
-                </tr>
-                <tr class="border-b bg-white hover:bg-slate-100">
-                    <th class="whitespace-nowrap px-6 py-4 font-medium text-gray-900" scope="row">
-                        23-MKU-CPL-02
-                    </th>
-                    <td class="px-6 py-4">
-                        Menjunjung tinggi nilai kemanusiaan, nasionalisme, kebangsaan, kepedulian terhadap masyarakat
-                        dan lingkungan, serta menghargai keanekaragaman budaya, pandangan, agama dan kepercayaan serta
-                        pendapat atau temuan orisinal orang lain
-                    </td>
-                    <td class="px-6 py-4">
-                        {{ $scoreCPL['23-MKU-CPL-02'] }}
-                    </td>
-                    <td class="px-6 py-4">
-                        {{ $scoreCPL['23-MKU-CPL-02'] >= 85
-                            ? 'Sangat kompeten (Exemplary)'
-                            : ($scoreCPL['23-MKU-CPL-02'] >= 75
-                                ? 'Kompeten (Competent)'
-                                : ($scoreCPL['23-MKU-CPL-02'] >= 60
-                                    ? 'Berkembang (Developing)'
-                                    : 'Tidak memuaskan (Unstatisfactory)')) }}
-                    </td>
-                    <td class="px-6 py-4">
-                        Sikap
-                    </td>
-                    <td class="px-6 py-4">
-                        <p>Nasionalisme</p>
-                        <br />
-                        <p>Peduli</p>
-                        <br />
-                        <p>Menghargai</p>
-                    </td>
-                </tr>
-                <tr class="border-b bg-white hover:bg-slate-100">
-                    <th class="whitespace-nowrap px-6 py-4 font-medium text-gray-900" scope="row">
-                        23-MKU-CPL-03
-                    </th>
-                    <td class="px-6 py-4">
-                        Menginternalisasi semangat kemandirian, kejuangan, dan kewirausahaan serta mampu menerapkan
-                        nilai-nilai Jaya sebagai university value
-                    </td>
-                    <td class="px-6 py-4">
-                        {{ $scoreCPL['23-MKU-CPL-03'] }}
-                    </td>
-                    <td class="px-6 py-4">
-                        {{ $scoreCPL['23-MKU-CPL-03'] >= 85
-                            ? 'Sangat kompeten (Exemplary)'
-                            : ($scoreCPL['23-MKU-CPL-03'] >= 75
-                                ? 'Kompeten (Competent)'
-                                : ($scoreCPL['23-MKU-CPL-03'] >= 60
-                                    ? 'Berkembang (Developing)'
-                                    : 'Tidak memuaskan (Unstatisfactory)')) }}
-                    </td>
-                    <td class="px-6 py-4">
-                        Sikap
-                    </td>
-                    <td class="px-6 py-4">
-                        <p>Kemandirian</p>
-                        <br />
-                        <p>Tata kelola diri</p>
-                    </td>
-                </tr>
-                <tr class="border-b bg-white hover:bg-slate-100">
-                    <th class="whitespace-nowrap px-6 py-4 font-medium text-gray-900" scope="row">
-                        23-MKU-CPL-04
-                    </th>
-                    <td class="px-6 py-4">
-                        Menerapkan pemikiran logis, kritis, sistematis, dan inovatif dalam konteks pengembangan atau
-                        implementasi ilmu pengetahuan dan teknologi yang memperhatikan dan menerapkan nilai humaniora
-                        yang sesuai dengan bidang keahliannya serta menunjukkan kinerja mandiri, bermutu, dan terukur
-                    </td>
-                    <td class="px-6 py-4">
-                        {{ $scoreCPL['23-MKU-CPL-04'] }}
-                    </td>
-                    <td class="px-6 py-4">
-                        {{ $scoreCPL['23-MKU-CPL-04'] >= 85
-                            ? 'Sangat kompeten (Exemplary)'
-                            : ($scoreCPL['23-MKU-CPL-04'] >= 75
-                                ? 'Kompeten (Competent)'
-                                : ($scoreCPL['23-MKU-CPL-04'] >= 60
-                                    ? 'Berkembang (Developing)'
-                                    : 'Tidak memuaskan (Unstatisfactory)')) }}
-                    </td>
-                    <td class="px-6 py-4">
-                        <p>Keterampilan Umum</p>
-                        <br>
-                        <div>Pengetahuan</div>
-                    </td>
-                    <td class="px-6 py-4">
-                        <p>Berpikir logis dan analitis</p>
-                        <br />
-                        <p>Pengelolaan tugas dan perbaikan berkelanjutan</p>
-                        <br />
-                        <p>Menyelesaikan masalah</p>
-                        <p>Peyusunan laporan kegiatan ilmiah</p>
-                    </td>
-                </tr>
+            <tbody class="text-base text-gray-900">
+                @foreach ($cpl as $item)
+                    @php
+                        // Ambil nilai skor CPL
+                        $score = $item['score'] ?? 0;
+
+                        // Tentukan predikat berdasarkan skor
+                        $predikat = 'Tidak memuaskan (Unstatisfactory)';
+                        foreach ($predikatCPL as $range) {
+                            [$min, $max] = explode(' - ', $range['range']);
+                            if ($score >= $min && $score <= $max) {
+                                $predikat = $range['predikat'];
+                                break;
+                            }
+                        }
+                    @endphp
+
+                    <tr class="border-b bg-white hover:bg-slate-100">
+                        <th class="whitespace-nowrap px-6 py-4" scope="row">
+                            {{ $item['kode_cpl'] }}
+                        </th>
+                        <td class="px-6 py-4">
+                            {{ $item['deskripsi_cpl'] }}
+                        </td>
+                        <td class="px-6 py-4">
+                            {{ number_format($score, 2) }}
+                        </td>
+                        <td class="px-6 py-4">
+                            {{ $predikat }}
+                        </td>
+                        <td class="px-6 py-4">
+                            {{ implode(', ', $item['aspek']) }}
+                        </td>
+                        <td class="w-full divide-y divide-gray-300">
+                            <div class="px-6 py-1">
+                                <ul class="divide-y divide-gray-300">
+                                    @foreach ($item['kemampuan'] as $kemampuan)
+                                        <li class="py-4">{{ $kemampuan }}</li>
+                                    @endforeach
+                                </ul>
+                            </div>
+                        </td>
+                    </tr>
+                @endforeach
             </tbody>
         </table>
     </div>
 
-</x-layouts.app-dashboard>
+</x-app-dashboard>
