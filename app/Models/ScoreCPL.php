572bfc02a414d9e9399a1ce3db6a919ae4a42312
danisec <daniaprilyanto35@gmail.com>
2024-08-05 15:54:28 +0700

    update: models/ScoreCPL

--- app/Models/ScoreCPL.php
+++ app/Models/ScoreCPL.php
@@ -2,14 +2,18 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class ScoreCPL extends Model
 {
-    use HasFactory;
+    use HasFactory, HasUuids;
 
     protected $table = 'score_cpl';
     protected $guarded = ['id_score_cpl'];
     protected $primaryKey = 'id_score_cpl';
+
+    protected $keyType = 'string';
+    public $incrementing = false;
 }
