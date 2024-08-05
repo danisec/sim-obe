e27fe0ab1b9d938e16f8bdf750c21915f77628e0
danisec <daniaprilyanto35@gmail.com>
2024-08-05 16:16:28 +0700

    update: routes/web

--- routes/web.php
+++ routes/web.php
@@ -28,6 +28,11 @@ Route::controller(LoginController::class)->name('login.')->group(function () {
     Route::post('/logout', 'logout')->name('logout')->middleware('auth');
 });
 
+Route::controller(RegisterController::class)->name('register.')->middleware('guest')->group(function () {
+    Route::get('/register', 'index')->name('index');
+    Route::post('/register', 'store')->name('store');
+});
+
 
 Route::controller(DashboardController::class)->name('dashboard.')->middleware('auth')->group(function () {
     Route::get('/dashboard', 'index')->name('index');
@@ -35,33 +40,46 @@ Route::controller(DashboardController::class)->name('dashboard.')->middleware('a
 
 Route::controller(CapaianPembelajaranLulusanController::class)->name('capaianPembelajaran.')->middleware('auth')->group(function () {
     Route::get('/dashboard/data-cpl-cpmk-scpmk', 'index')->name('index');
+
     Route::get('/dashboard/data-cpl-cpmk-scpmk/import', 'import')->name('import');
+    Route::post('/dashboard/data-cpl-cpmk-scpmk/import', 'store')->name('store');
+
     Route::get('/dashboard/data-cpl-cpmk-scpmk/{id}/view-cpl-cpmk-scpmk', 'show')->name('show');
 
-    Route::post('/dashboard/data-cpl-cpmk-scpmk/import', 'store')->name('store');
     Route::delete('/dashboard/data-cpl-cpmk-scpmk/{id}', 'destroy')->name('destroy');
 });
 
 Route::controller(MappingCapaianPembelajaranLulusanController::class)->name('mappingCpl.')->middleware('auth')->group(function () {
     Route::get('/dashboard/data-mapping-cpl', 'index')->name('index');
+
     Route::get('/dashboard/data-mapping-cpl/create-mapping-cpl', 'create')->name('create');
+    Route::post('/dashboard/data-mapping-cpl/create-mapping-cpl', 'store')->name('store');
+
     Route::get('/dashboard/data-mapping-cpl/{id}/view-mapping-cpl', 'show')->name('show');
 
-    Route::post('/dashboard/data-mapping-cpl/create-mapping-cpl', 'store')->name('store');
+    Route::get('/dashboard/data-mapping-cpl/ubah-mapping-cpl/{id}/edit', 'edit')->name('edit');
+    Route::put('/dashboard/data-mapping-cpl/{id}', 'update')->name('update');
+
     Route::delete('/dashboard/data-mapping-cpl/{id}', 'destroy')->name('destroy');
+
+    Route::get('/dashboard/data-mapping-cpl/create-mapping-cpl/{kodeMataKuliah}/getNamaMataKuliah', 'getNamaMataKuliah')->name('getNamaMataKuliah');
 
     Route::get('/dashboard/data-mapping-cpl/create-mapping-cpl/{kodeCpl}/getCpmk', 'getCpmk')->name('getCpmk');
     Route::get('/dashboard/data-mapping-cpl/create-mapping-cpl/{kodeCpmk}/getScpmk', 'getScpmk')->name('getScpmk');
+
+    Route::get('/dashboard/data-mapping-cpl/create-mapping-cpl/{kodeMataKuliah}/getIndikatorScpmk', 'getIndikatorScpmk')->name('getIndikatorScpmk');
 });
 
 Route::controller(HasilPembelajaranController::class)->name('hasilPembelajaran.')->middleware('auth')->group(function () {
     Route::get('/dashboard/data-hasil-pembelajaran', 'index')->name('index');
+
     Route::get('/dashboard/data-hasil-pembelajaran/create-hasil-pembelajaran', 'create')->name('create');
+    Route::post('/dashboard/data-hasil-pembelajaran/create-hasil-pembelajaran', 'store')->name('store');
+    
     Route::get('/dashboard/data-hasil-pembelajaran/{id}/view-hasil-pembelajaran', 'show')->name('show');
 
     Route::get('/dashboard/data-hasil-pembelajaran/create-hasil-pembelajaran/{kodeMataKuliah}/getMataKuliah', 'getMataKuliah')->name('getMataKuliah');
 
-    Route::post('/dashboard/data-hasil-pembelajaran/create-hasil-pembelajaran', 'store')->name('store');
     Route::post('/dashboard/data-hasil-pembelajaran', 'storeScore')->name('storeScore');
     
     Route::delete('/dashboard/data-hasil-pembelajaran/{id}', 'destroy')->name('destroy');
