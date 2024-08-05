a87c316444f684ce0944d56e592e90ef4d14d42b
danisec <daniaprilyanto35@gmail.com>
2024-08-05 16:05:49 +0700

    update: js/mappingCpl/app

--- resources/js/mappingCpl/app.js
+++ resources/js/mappingCpl/app.js
@@ -2,12 +2,32 @@ if (
     window.location.pathname ===
     "/dashboard/data-mapping-cpl/create-mapping-cpl"
 ) {
+    import("./getMataKuliah")
+        .then((module) => {})
+        .catch((error) => {
+            console.error("Gagal mengimpor getMataKuliah");
+        });
+
     import("./getCpmk")
         .then((module) => {})
         .catch((error) => {
             console.error("Gagal mengimpor getCpmk");
         });
 
+    import("./getIndikatorScpmk")
+        .then((module) => {})
+        .catch((error) => {
+            console.error("Gagal mengimpor getIndikatorScpmk");
+        });
+}
+
+if (
+    window.location.pathname ===
+        "/dashboard/data-mapping-cpl/create-mapping-cpl" ||
+    window.location.pathname.includes(
+        "/dashboard/data-mapping-cpl/ubah-mapping-cpl/",
+    )
+) {
     import("./calculateBobot")
         .then((module) => {})
         .catch((error) => {
