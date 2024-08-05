32165eb7462c3abdb6cc97b94c5f1d184ff88ea0
danisec <daniaprilyanto35@gmail.com>
2024-08-05 15:50:28 +0700

    update: ImportData

--- app/Imports/ImportData.php
+++ app/Imports/ImportData.php
@@ -4,6 +4,7 @@ namespace App\Imports;
 
 use App\Models\CapaianPembelajaranLulusan;
 use App\Models\CapaianPembelajaranMataKuliah;
+use App\Models\MataKuliah;
 use App\Models\SubCapaianPembelajaranMataKuliah;
 use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\DB;
@@ -15,6 +16,7 @@ class ImportData implements ToCollection, WithHeadingRow
 {
     protected $previousCplId = null;
     protected $previousCpmkId = null;
+    protected $previousScpmkId = null;
 
     public function collection(Collection $rows)
     {
@@ -45,11 +47,26 @@ class ImportData implements ToCollection, WithHeadingRow
 
                 // Import ke tabel SubCapaianPembelajaranMataKuliah
                 if (!empty($row['kode_scpmk'])) {
-                    SubCapaianPembelajaranMataKuliah::updateOrCreate(
+                    $scpmk = SubCapaianPembelajaranMataKuliah::updateOrCreate(
                         ['kode_scpmk' => $row['kode_scpmk']],
                         [
                             'id_cpmk' => $cpmk->id_cpmk, // Pastikan $cpmk telah didefinisikan di atas
-                            'deskripsi_scpmk' => $row['deskripsi_scpmk']
+                            'deskripsi_scpmk' => $row['deskripsi_scpmk'],
+                            'kemampuan' => $row['kemampuan'],
+                            'aspek' => $row['aspek'],
+                        ]
+                    );
+
+                    $this->previousScpmkId = $scpmk->id_scpmk; // Simpan id_scpmk untuk digunakan di baris berikutnya
+                }
+
+                // Import ke tabel MataKuliah secara multi insert berdasarkan id_scpmk
+                if (!empty($this->previousScpmkId)) {
+                    MataKuliah::updateOrCreate(
+                        [
+                            'id_scpmk' => $this->previousScpmkId,
+                            'kode_mata_kuliah' => $row['kode_mata_kuliah'],
+                            'nama_mata_kuliah' => $row['nama_mata_kuliah'],
                         ]
                     );
                 }
