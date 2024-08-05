74e58d19ef6fee88f07ca96b92d0a351d22e6e28
danisec <daniaprilyanto35@gmail.com>
2024-08-05 16:15:26 +0700

    update: pages/dashboard/profile/settings

--- resources/views/pages/dashboard/profile/settings.blade.php
+++ resources/views/pages/dashboard/profile/settings.blade.php
@@ -1,4 +1,4 @@
-<x-layouts.app-dashboard title="{{ $title }}">
+<x-app-dashboard title="{{ $title }}">
 
     <x-molecules.breadcrumb>
         <li aria-current="page">
@@ -99,4 +99,4 @@
         </div>
     </div>
 
-</x-layouts.app-dashboard>
+</x-app-dashboard>
