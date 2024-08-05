164c6f3f0bf161e84656523f2510782ff09e49be
danisec <daniaprilyanto35@gmail.com>
2024-08-05 16:13:48 +0700

    update: pages/dashboard/hasil-pembelajaran/show

--- resources/views/pages/dashboard/hasil-pembelajaran/show.blade.php
+++ resources/views/pages/dashboard/hasil-pembelajaran/show.blade.php
@@ -1,4 +1,4 @@
-<x-layouts.app-dashboard title="{{ $title }}">
+<x-app-dashboard title="{{ $title }}">
 
     <x-molecules.breadcrumb>
         <li aria-current="page">
@@ -108,4 +108,4 @@
         </div>
     </div>
 
-</x-layouts.app-dashboard>
+</x-app-dashboard>
