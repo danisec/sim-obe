a44d7901ef53ffcf174b96dc188fedac5aeae41c
danisec <daniaprilyanto35@gmail.com>
2024-08-05 15:53:15 +0700

    update: models/CapaianPembelajaranMataKuliah

--- app/Models/CapaianPembelajaranMataKuliah.php
+++ app/Models/CapaianPembelajaranMataKuliah.php
@@ -2,16 +2,20 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class CapaianPembelajaranMataKuliah extends Model
 {
-    use HasFactory;
+    use HasFactory, HasUuids;
 
     protected $table = 'capaian_pembelajaran_mata_kuliah';
     protected $guarded = ['id_cpmk'];
     protected $primaryKey = 'id_cpmk';
+
+    protected $keyType = 'string';
+    public $incrementing = false;
 
     public function capaianPembelajaranLulusan()
     {
