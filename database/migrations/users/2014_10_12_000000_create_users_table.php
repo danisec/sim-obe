cbaccffffaf331ee95d10a8d007711ad15786469
danisec <daniaprilyanto35@gmail.com>
2024-08-05 16:03:20 +0700

    update: migrations/user

--- database/migrations/users/2014_10_12_000000_create_users_table.php
+++ database/migrations/users/2014_10_12_000000_create_users_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
+            $table->uuid('id')->primary();
             $table->string('fullname', 255)->index();
             $table->string('username', 50)->unique();
             $table->string('email')->unique();
