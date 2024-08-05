f56a0ad9d5800f9e064b43863469331798655136
danisec <daniaprilyanto35@gmail.com>
2024-08-05 15:54:45 +0700

    update: models/SubCapaianPembelajaranMataKuliah

--- app/Models/SubCapaianPembelajaranMataKuliah.php
+++ app/Models/SubCapaianPembelajaranMataKuliah.php
@@ -2,19 +2,28 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class SubCapaianPembelajaranMataKuliah extends Model
 {
-    use HasFactory;
+    use HasFactory, HasUuids;
 
     protected $table = 'sub_capaian_pembelajaran_mata_kuliah';
     protected $guarded = ['id_scpmk'];
     protected $primaryKey = 'id_scpmk';
 
+    protected $keyType = 'string';
+    public $incrementing = false;
+
     public function capaianPembelajaranMataKuliah()
     {
         return $this->belongsTo(CapaianPembelajaranMataKuliah::class, 'id_cpmk', 'id_cpmk');
+    }
+
+    public function mataKuliah()
+    {
+        return $this->hasMany(MataKuliah::class, 'id_scpmk', 'id_scpmk');
     }
 }
