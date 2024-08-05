a8ad95667f41e411a0d63443d4d5e81e33d279a7
danisec <daniaprilyanto35@gmail.com>
2024-08-05 16:13:41 +0700

    update: pages/dashboard/hasil-pembelajaran/index

--- resources/views/pages/dashboard/hasil-pembelajaran/index.blade.php
+++ resources/views/pages/dashboard/hasil-pembelajaran/index.blade.php
@@ -1,4 +1,4 @@
-<x-layouts.app-dashboard title="{{ $title }}">
+<x-app-dashboard title="{{ $title }}">
 
     <x-molecules.breadcrumb>
         <li aria-current="page">
@@ -107,170 +107,58 @@
                     <th class="px-6 py-3" scope="col">
                         Nama Mata Kuliah
                     </th>
-                    <th class="px-6 py-3" scope="col">
-                        23-MKU-CPL-01
-                    </th>
-                    <th class="px-6 py-3" scope="col">
-                        23-MKU-CPL-02
-                    </th>
-                    <th class="px-6 py-3" scope="col">
-                        23-MKU-CPL-03
-                    </th>
-                    <th class="px-6 py-3" scope="col">
-                        23-MKU-CPL-04
-                    </th>
+                    @foreach ($allCplCodes as $cplCode)
+                        <th class="px-6 py-3" scope="col">
+                            {{ $cplCode }}
+                        </th>
+                    @endforeach
                 </tr>
             </thead>
 
             <tbody>
-                <tr class="border-b bg-white hover:bg-slate-100">
-                    <th class="whitespace-nowrap px-6 py-4 font-medium text-gray-900" scope="row">
-                        MKU101
-                    </th>
-                    <td class="px-6 py-4">
-                        Agama
-                    </td>
-                    <td class="px-6 py-4">
-                        {{ isset($totalHasilPembelajaran['MKU101']) ? number_format($totalHasilPembelajaran['MKU101'] / 100, 2) : '' }}
-                    </td>
-                    <td class="px-6 py-4">
-                        {{ isset($totalHasilPembelajaran['MKU101']) ? number_format($totalHasilPembelajaran['MKU101'] / 100, 2) : '' }}
-                    </td>
-                    <td class="px-6 py-4">
-                    </td>
-                    <td class="px-6 py-4">
-                    </td>
-                </tr>
-                <tr class="border-b bg-white hover:bg-slate-100">
-                    <th class="whitespace-nowrap px-6 py-4 font-medium text-gray-900" scope="row">
-                        MKU102
-                    </th>
-                    <td class="px-6 py-4">
-                        Pancasila dan Kewarganegaraan
-                    </td>
-                    <td class="px-6 py-4">
-                        {{ isset($totalHasilPembelajaran['MKU102']) ? number_format($totalHasilPembelajaran['MKU102'] / 100, 2) : '' }}
-                    </td>
-                    <td class="px-6 py-4">
-                        {{ isset($totalHasilPembelajaran['MKU102']) ? number_format($totalHasilPembelajaran['MKU102'] / 100, 2) : '' }}
-                    </td>
-                    <td class="px-6 py-4">
-                    </td>
-                    <td class="px-6 py-4">
-                    </td>
-                </tr>
-                <tr class="border-b bg-white hover:bg-slate-100">
-                    <th class="whitespace-nowrap px-6 py-4 font-medium text-gray-900" scope="row">
-                        MKU103
-                    </th>
-                    <td class="px-6 py-4">
-                        Bahasa Indonesia
-                    </td>
-                    <td class="px-6 py-4">
-                    </td>
-                    <td class="px-6 py-4">
-                        {{ isset($totalHasilPembelajaran['MKU103']) ? number_format($totalHasilPembelajaran['MKU103'] / 100, 2) : '' }}
-                    </td>
-                    <td class="px-6 py-4">
-                    </td>
-                    <td class="px-6 py-4">
-                        {{ isset($totalHasilPembelajaran['MKU103']) ? number_format($totalHasilPembelajaran['MKU103'] / 100, 2) : '' }}
-                    </td>
-                </tr>
-                <tr class="border-b bg-white hover:bg-slate-100">
-                    <th class="whitespace-nowrap px-6 py-4 font-medium text-gray-900" scope="row">
-                        MKU104
-                    </th>
-                    <td class="px-6 py-4">
-                        Pembangunan Berkelanjutan
-                    </td>
-                    <td class="px-6 py-4">
-                    </td>
-                    <td class="px-6 py-4">
-                        {{ isset($totalHasilPembelajaran['MKU104']) ? number_format($totalHasilPembelajaran['MKU104'] / 100, 2) : '' }}
-                    </td>
-                    <td class="px-6 py-4">
-                        {{ isset($totalHasilPembelajaran['MKU104']) ? number_format($totalHasilPembelajaran['MKU104'] / 100, 2) : '' }}
-                    </td>
-                    <td class="px-6 py-4">
-                        {{ isset($totalHasilPembelajaran['MKU104']) ? number_format($totalHasilPembelajaran['MKU104'] / 100, 2) : '' }}
-                    </td>
-                </tr>
-                <tr class="border-b bg-white hover:bg-slate-100">
-                    <th class="whitespace-nowrap px-6 py-4 font-medium text-gray-900" scope="row">
-                        MKU105
-                    </th>
-                    <td class="px-6 py-4">
-                        Wawasan Kewirausahaan
-                    </td>
-                    <td class="px-6 py-4">
-                    </td>
-                    <td class="px-6 py-4">
-                    </td>
-                    <td class="px-6 py-4">
-                        {{ isset($totalHasilPembelajaran['MKU105']) ? number_format($totalHasilPembelajaran['MKU105'] / 100, 2) : '' }}
-                    </td>
-                    <td class="px-6 py-4">
-                        {{ isset($totalHasilPembelajaran['MKU105']) ? number_format($totalHasilPembelajaran['MKU105'] / 100, 2) : '' }}
-                    </td>
-                </tr>
-                <tr class="border-b bg-white hover:bg-slate-100">
-                    <th class="whitespace-nowrap px-6 py-4 font-medium text-gray-900" scope="row">
-                        MKU106
-                    </th>
-                    <td class="px-6 py-4">
-                        Dasar Logika Matematika
-                    </td>
-                    <td class="px-6 py-4">
-                    </td>
-                    <td class="px-6 py-4">
-                    </td>
-                    <td class="px-6 py-4">
-                        {{ isset($totalHasilPembelajaran['MKU106']) ? number_format($totalHasilPembelajaran['MKU106'] / 100, 2) : '' }}
-                    </td>
-                    <td class="px-6 py-4">
-                        {{ isset($totalHasilPembelajaran['MKU106']) ? number_format($totalHasilPembelajaran['MKU106'] / 100, 2) : '' }}
-                    </td>
-                </tr>
-                <tr class="border-b bg-white hover:bg-slate-100">
-                    <th class="whitespace-nowrap px-6 py-4 font-medium text-gray-900" scope="row">
-                        MKU107
-                    </th>
-                    <td class="px-6 py-4">
-                        English for Specific Purposes
-                    </td>
-                    <td class="px-6 py-4">
-                    </td>
-                    <td class="px-6 py-4">
-                        {{ isset($totalHasilPembelajaran['MKU107']) ? number_format($totalHasilPembelajaran['MKU107'] / 100, 2) : '' }}
-                    </td>
-                    <td class="px-6 py-4">
-                        {{ isset($totalHasilPembelajaran['MKU107']) ? number_format($totalHasilPembelajaran['MKU107'] / 100, 2) : '' }}
-                    </td>
-                    <td class="px-6 py-4">
-                    </td>
-                </tr>
+                @foreach ($hasilPembelajaran as $item)
+                    <tr class="border-b bg-white hover:bg-slate-100">
+                        <th class="whitespace-nowrap px-6 py-4 font-medium text-gray-900" scope="row">
+                            {{ $item->kode_mata_kuliah }}
+                        </th>
+                        <td class="px-6 py-4">
+                            {{ $item->nama_mata_kuliah }}
+                        </td>
+                        @foreach ($allCplCodes as $cplCode)
+                            <td class="px-6 py-4">
+                                @php
+                                    $kodeMK = $item->kode_mata_kuliah;
+                                    // Memeriksa apakah kode mata kuliah memiliki data hasil pembelajaran
+                                    $value = isset($totalHasilPembelajaran[$kodeMK])
+                                        ? $totalHasilPembelajaran[$kodeMK]
+                                        : 0;
+                                    // Memeriksa apakah kode CPL ada dalam kodeCPL untuk mata kuliah ini
+                                    $cplValues = $kodeCPL[$kodeMK] ?? [];
+                                    echo in_array($cplCode, $cplValues) ? number_format($value / 100, 2) : '';
+                                @endphp
+                            </td>
+                        @endforeach
+                    </tr>
+                @endforeach
+
+                @foreach ($allCplCodes as $index => $cplCode)
+                    <input class="cpl-code" id="cpl-code-{{ $index }}" type="hidden"
+                        value="{{ $cplCode }}">
+                @endforeach
+
                 <tr class="border-b bg-slate-100 font-semibold text-gray-900 hover:bg-slate-100">
-                    <th class="whitespace-nowrap px-6 py-4" scope="row">
-                    </th>
-                    <td class="px-6 py-4">
+                    <th class="whitespace-nowrap px-6 py-4 text-center" scope="row" colspan="2">
                         Score CPL (Rata-rata)
-                    </td>
-                    <td class="px-6 py-4" id="rata-kolom-1">
-                        <input id="input-rata-kolom-1" name="rata_kolom_1" type="hidden">
-                    </td>
-                    <td class="px-6 py-4" id="rata-kolom-2">
-                        <input id="input-rata-kolom-2" name="rata_kolom_2" type="hidden">
-                    </td>
-                    <td class="px-6 py-4" id="rata-kolom-3">
-                        <input id="input-rata-kolom-3" name="rata_kolom_3" type="hidden">
-                    </td>
-                    <td class="px-6 py-4" id="rata-kolom-4">
-                        <input id="input-rata-kolom-4" name="rata_kolom_4" type="hidden">
-                    </td>
+                    </th>
+                    @foreach ($allCplCodes as $index => $cplCode)
+                        <td class="px-6 py-4" id="rata-kolom-{{ $index + 1 }}">
+                            <input id="input-rata-kolom-{{ $index + 1 }}" name="rata_kolom_{{ $index + 1 }}"
+                                type="hidden">
+                        </td>
+                    @endforeach
                 </tr>
             </tbody>
         </table>
     </div>
 
-</x-layouts.app-dashboard>
+</x-app-dashboard>
