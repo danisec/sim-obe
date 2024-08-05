c739fb69e50bdfe7975643da7881328987a0b342
danisec <daniaprilyanto35@gmail.com>
2024-08-05 15:55:04 +0700

    update: models/TotalHasilPembelajaran

--- app/Models/TotalHasilPembelajaran.php
+++ app/Models/TotalHasilPembelajaran.php
@@ -2,16 +2,20 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class TotalHasilPembelajaran extends Model
 {
-    use HasFactory;
+    use HasFactory, HasUuids;
 
     protected $table = 'total_hasil_pembelajaran';
     protected $guarded = ['id_total_hasil_pembelajaran'];
     protected $primaryKey = 'id_total_hasil_pembelajaran';
+
+    protected $keyType = 'string';
+    public $incrementing = false;
 
     public function hasilPembelajaran()
     {
