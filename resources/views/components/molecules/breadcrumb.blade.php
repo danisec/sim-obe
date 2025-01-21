18c5679132754f9a92ef971e66a27cc9e070386c
danisec <daniaprilyanto35@gmail.com>
2025-01-21 15:36:10 +0700

    update: breadcrumbs

--- resources/views/components/molecules/breadcrumb.blade.php
+++ resources/views/components/molecules/breadcrumb.blade.php
@@ -1,8 +1,9 @@
-<nav class="my-2 flex justify-between text-gray-900" aria-label="Breadcrumb">
+<nav class="no-scrollbar my-2 flex justify-between overflow-x-auto whitespace-nowrap text-gray-900"
+    aria-label="Breadcrumb">
     <ol class="mb-3 inline-flex items-center space-x-3">
         <li>
             <div class="flex items-center">
-                <a class="text-base font-medium text-gray-900 hover:text-blue-600"
+                <a class="text-sm font-medium text-gray-900 hover:text-blue-600 md:text-base"
                     href="{{ route('dashboard.index') }}">Dashboard</a>
             </div>
         </li>
